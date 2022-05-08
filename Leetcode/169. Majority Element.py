22ce72899ec3e94dfd09567879347a5854df209a
PankajBora <pankajbora1111@gmail.com>
2022-05-08 11:52:36 +0530

    modified

--- Leetcode/169. Majority Element.py
+++ Leetcode/169. Majority Element.py
@@ -17,3 +17,29 @@ for key in hmap:
         break
 
 print(ans)
+
+
+
+
+'''
+
+time = O(n)
+space = O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        val = 0
+        for i in nums:
+            if count == 0:
+                val = i
+                
+            if val == i:
+                count += 1
+            
+            else :
+                count -= 1
+                    
+        return val
+        
+'''
